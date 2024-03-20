3e5d67c6eaefb15077552b05a954bb76f2071e1f
lvlnsiva <sivaprasad.n@bitlasoft.com>
2024-03-20 22:33:13 +0530

    test cases added

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,6 +3,17 @@ module StringCalculator
   def self.add(input)
     raise 'only accepts a string' unless input.is_a?(String)
     numbers = input.scan(/[0-9-]+/).map(&:to_i)
+    if negatives = get_negatives(numbers)
+      raise "negative numbers not allowed - #{negatives}"
+    end
     numbers.inject(0){|sum, x| x <= 1000 ? sum + x : sum }
   end
+
+  private
+
+  def get_negatives(numbers)
+    negatives = numbers.select{ |i| i < 0 }
+    negatives.join(",") if negatives.length > 0
+  end
+
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe StringCalculator, "#add" do
   it 'should return 0 for an empty string' do
     expect(StringCalculator.add('')).to eq(0)
   end
+
+  it 'should return a number if the passed string contains no delimiters' do
+    expect(StringCalculator.add('123')).to eq (123)
+  end
+
+  it 'should return the sum of the numbers in the passed string, if the passed 
+string contains comma delimiters' do
+    expect(StringCalculator.add('12,34')).to eq(46)
+  end
+
+  it 'should return the sum of the numbers in the passed string, if the passed 
+string contains new line delimiters' do
+    expect(StringCalculator.add("12\n34\n56")).to eq(102)
+  end
+
+  it 'should handle multiple random delimiters' do
+    expect(StringCalculator.add("//;\n1;2")).to eq(3)
+  end
+
+  it 'should not accept negative numbers' do
+    expect{ StringCalculator.add("123,-2") }.to raise_error("negative numbers not allowed - -2")
+  end
+
+  it 'should ignore numbers larger than 1000' do
+    expect(StringCalculator.add("//;\n1;2:1001")).to eq(3)
+  end
 end
